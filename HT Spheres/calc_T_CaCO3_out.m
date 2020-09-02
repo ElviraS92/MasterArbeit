ba3bce9a865bcd24a4bf4a3b677ae658938432c6
ElviraS <elvira.sessler@gmail.com>
2020-09-02 11:59:38 +0200

    Bugfix Q Wärmefluss

--- HT Spheres/calc_T_CaCO3_out.m
+++ HT Spheres/calc_T_CaCO3_out.m
@@ -1,6 +1,6 @@
-function [T_CaCO3_out] = calc_T_CaCO3_out(Q, T_CaCO3_in, cp_CaCO3, m_2, t)
+function [T_CaCO3_out] = calc_T_CaCO3_out(Q, T_CaCO3_in, cp_CaCO3, m_2)
 %CALC_T_CACO3_OUT Summary of this function goes here
 %   Detailed explanation goes here
-T_CaCO3_out=T_CaCO3_in+(Q/(cp_CaCO3*m_2*t));
+T_CaCO3_out=T_CaCO3_in+(Q/(cp_CaCO3*m_2));
 end
 
--- HT Spheres/calc_T_HTS_out_3.m
+++ HT Spheres/calc_T_HTS_out_3.m
@@ -1,8 +1,8 @@
-function [T_HTS_out] = calc_T_HTS_out_3(Q, T_HTS_in, cp_HTS, m_HTS, t)
+function [T_HTS_out] = calc_T_HTS_out_3(Q, T_HTS_in, cp_HTS, m_HTS)
 %CALC_T_HTS_OUT Summary of this function goes here
 %   Detailed explanation goes here
 
-T_HTS_out=T_HTS_in-(Q/(cp_HTS*m_HTS*t));
+T_HTS_out=T_HTS_in-(Q/(cp_HTS*m_HTS));
 
 end
 
