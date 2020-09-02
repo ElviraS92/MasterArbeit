d2d345e609da360f52a55d0a703e99e36ee0759d
ElviraS <elvira.sessler@gmail.com>
2020-09-02 18:55:03 +0200

    Wärmestrom Q in J/s (ohne t_res)

--- HT Spheres/calc_Q_hx_CaCO3.m
+++ HT Spheres/calc_Q_hx_CaCO3.m
@@ -2,7 +2,7 @@ function [Q] = calc_Q_hx_CaCO3(alpha, T_HTS_in, T_CaCO3_in, A_hx_tot, t)
 %CALC_Q_HX_CACO3 Summary of this function goes here
 %   Detailed explanation goes here
 
-Q=alpha*t*A_hx_tot*(T_HTS_in-T_CaCO3_in);
+Q=alpha*A_hx_tot*(T_HTS_in-T_CaCO3_in);
 
 end
 
--- HT Spheres/calc_Q_hx_CaO.m
+++ HT Spheres/calc_Q_hx_CaO.m
@@ -2,7 +2,7 @@ function [Q] = calc_Q_hx_CaO(alpha, T_HTS_in, T_CaO_in, A_hx_tot, t)
 %CALC_Q_HX_CACO3 Summary of this function goes here
 %   Detailed explanation goes here
 
-Q=alpha*t*A_hx_tot*(T_CaO_in-T_HTS_in);
+Q=alpha*A_hx_tot*(T_CaO_in-T_HTS_in);
 
 end
 
