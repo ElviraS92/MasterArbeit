72e220eb37b0a93dea4482e9b9a429c1e7420cb3
ElviraS <elvira.sessler@gmail.com>
2020-09-02 18:40:00 +0200

    Archiv verschoben

--- HT Spheres/archiv/cohesion_paused/calc_N_c11_coh.m
+++ HT Spheres/archiv/cohesion_paused/calc_N_c11_coh.m
@@ -1,6 +1,6 @@
 function [N_c11] = calc_N_c11(phi_C, d_p)
 %CALC_N_C11: N_c11 - Coordination Number, contacts of large particles to another large particle, here  p1= HTS [-] 
-%            phi_C - angle of internal friction [°]
+%            phi_C - angle of internal friction [ï¿½]
 %   Detailed explanation goes here
 
 % N_c11=(0.804-1.07*sin(phi_C*pi/180).^2)*((d_p/d_p)+1)/(1+(d_p/d_p)-sqrt((d_p/d_p)*(d_p/d_p+2)));
